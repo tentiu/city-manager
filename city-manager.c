a704772a0b04cbcc8279d6c1a6ee15a491785d54
tentiu <118485151+tentiu@users.noreply.github.com>
2026-04-15 11:36:49 +0300

    Update city-manager.c

--- city-manager.c
+++ city-manager.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+
+void print_function(){
+    printf("test");
+}
+
+void add_function(char *district_id){
+
+}
 
 int main(int argc, char **argv){
-
+    //int district_id;
+    //int report_id;
+    if(argc==1){
+        print_function();
+    }
     return 0;
 }
